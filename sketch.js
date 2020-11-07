49ee74f095113bff425f7fb3325c6b18af0fb0f1
blue-eagle-089 <73334670+blue-eagle-089@users.noreply.github.com>
2020-11-07 15:32:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+var car,wall;
+
+var speed,weight
+
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed=random(59,90)
+  weight=random(400,1500)
+  
+  car = createSprite(50,200,50,50)
+  car.shapeColor = "255"
+  car.velocityX = speed;
+
+  wall = createSprite(1300,200,60,200);
+  wall.shapeColor = "grey"
+
+  
+  
+}
+
+function draw() {
+  background("black"); 
+  
+  if(car.isTouching(wall))
+  {
+    car.velocityX = 0;
+    var deformation = 0.5 * weight * speed * speed/22509
+    
+    if (deformation>180)
+    {
+      car.shapeColor = color(255,0,0)
+    }
+    if (deformation<180 && deformation>100)
+    {
+      car.shapeColor = color(230,230,0)
+    }
+    if(deformation<100)
+    {
+      car.shapeColor = color(0,255,0)
+    } 
+  }
+
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
